3584aed7ba4bb3de13e59553a653f67391f16d18
KilianBrd <kilianberaud3@gmail.com>
2026-03-04 18:07:03 +0100

    - Ajout d'un Middleware pour les actions administrateur.

---
+++ app/Http/Middleware/CheckIfOwnerKapsule.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Kapsule;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckIfOwnerKapsule
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $kapsule = Kapsule::findOrFail($request->route('kapsule'));
+        $user = $request->user();
+
+        if ($kapsule->user_id !== $user->id) {
+            abort(403, __('you_are_not_the_owner_of_this_kapsule'));
+        }
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,11 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\KapsuleController;
 use App\Http\Controllers\MediaController;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UsersBannedKapsuleController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('login');
@@ -30,13 +28,15 @@ Route::middleware('auth')->group(function () {
         Route::get('/kapsule/{kapsule}', [KapsuleController::class, 'load'])
             ->name('kapsule.load');
     });
-    Route::post('/media/upload', [MediaController::class , 'store'])->name('media.upload');
-    Route::post('/kapsules/{kapsule}/join', [KapsuleController::class , 'join'])->name('kapsules.join');
-    Route::post('/kapsules/{kapsule}/accept/{member}', [KapsuleController::class , 'accept'])->name('kapsules.accept');
-    Route::post('/kapsules/{kapsule}/reject/{member}', [KapsuleController::class , 'reject'])->name('kapsules.reject');
-    Route::post('/kapsules/{kapsule}/ban/{member}', [KapsuleController::class , 'ban'])->name('kapsules.ban');
-    Route::get('/kapsules/{kapsule}/banned-users', [UsersBannedKapsuleController::class, 'index'])->name('kapsules.banned-users');
-    Route::post('/kapsules/{kapsule}/unban/{member}', [UsersBannedKapsuleController::class , 'unban'])->name('kapsules.unban');
+    Route::post('/media/upload', [MediaController::class, 'store'])->name('media.upload');
+    Route::post('/kapsules/{kapsule}/join', [KapsuleController::class, 'join'])->name('kapsules.join');
+    Route::post('/kapsules/{kapsule}/accept/{member}', [KapsuleController::class, 'accept'])->name('kapsules.accept');
+    Route::post('/kapsules/{kapsule}/reject/{member}', [KapsuleController::class, 'reject'])->name('kapsules.reject');
+    Route::middleware('App\Http\Middleware\CheckIfOwnerKapsule')->group(function () {
+        Route::post('/kapsules/{kapsule}/ban/{member}', [KapsuleController::class, 'ban'])->name('kapsules.ban');
+        Route::get('/kapsules/{kapsule}/banned-users', [UsersBannedKapsuleController::class, 'index'])->name('kapsules.banned-users');
+        Route::post('/kapsules/{kapsule}/unban/{member}', [UsersBannedKapsuleController::class, 'unban'])->name('kapsules.unban');
+    });
 });
 
 require __DIR__.'/auth.php';
