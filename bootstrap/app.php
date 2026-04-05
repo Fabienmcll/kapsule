22ba6b9b72af8053352bde160c45b7e702577b65
Fabienmcll <fabienmclll@gmail.com>
2026-04-06 00:51:40 +0200

    - Correctifs de sécu.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,14 +10,19 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
+        // On force les headers de sécurité même sur les pages d'erreur
+        $exceptions->respond(function ($response) {
+            $response->headers->set('X-Content-Type-Options', 'nosniff');
+            $response->headers->set('X-Frame-Options', 'DENY');
+            return $response;
+        });
         //
     })->create();
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,8 +10,7 @@
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600|outfit:400,500,600,700&display=swap"
-        rel="stylesheet" />
+    <link rel="stylesheet" href="https://fonts.bunny.net/css?family=figtree:400,500,600%7Coutfit:400,500,600,700&display=swap" integrity="sha384-sSmZjwp6a2aVWzQpqQlkWhVQDPEkwY1BUvajjp/Dj3bYSMnasgBs0NaW+V/f3sOu" crossorigin="anonymous">
 
     <!-- Scripts -->
     @routes
