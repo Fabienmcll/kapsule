71c5cea35bb911805df4fe7950cf0c7f853b943b
Fabienmcll <fabienmclll@gmail.com>
2026-04-06 18:22:51 +0200

    - Force le https en prod.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
@@ -29,5 +30,9 @@ class AppServiceProvider extends ServiceProvider
                 $media->user_id = Auth::id();
             }
         });
+
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
