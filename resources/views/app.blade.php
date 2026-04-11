20fb68df257145d4b1763188f2856a6dd4812936
Fabienmcll <fabienmclll@gmail.com>
2026-04-11 21:30:00 +0200

    - Ajout d ela progressive web app. - Ajout du logo et du nom Kapsule dans les onglets.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="manifest" href="{{ asset('manifest.json') }}">
+    <link rel="icon" type="image/png" href="{{ asset('icon-192.png') }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
 
     <!-- Scripts -->
@@ -17,6 +18,15 @@
 
 <body class="font-sans antialiased">
     @inertia
+    <script>
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', () => {
+                navigator.serviceWorker.register('/sw.js')
+                    .then(reg => console.log('SW enregistré !', reg))
+                    .catch(err => console.log('Erreur SW', err));
+            });
+        }
+    </script>
 </body>
 
 </html>
